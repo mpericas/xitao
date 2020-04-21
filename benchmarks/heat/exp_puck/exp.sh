a27f2713153cc4867ad9f62bb5cd98dd457988d4
Mustafa <musbar@gmail.com>
2020-04-21 13:46:57 +0200

    update experiments

--- benchmarks/heat/exp_puck/exp.sh
+++ benchmarks/heat/exp_puck/exp.sh
@@ -1,4 +1,45 @@
 XITAO_PTT_PATH=/home/musabdu/xitao/ptt_layouts/puck
+INPUTFILE=vsmall.dat
+
+echo "****************************************************************************************************"
+echo "RWSS"
+echo "****************************************************************************************************"
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=8 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-rwss ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Aware"
+echo "****************************************************************************************************"
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=8 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-No-Mold"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_1
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=8 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-Mold-2"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_2
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=8 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-Mold-6"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_6
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=8 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-Mold-1-2-6"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_1_2_6
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=8 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-Mold-1-2-6"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_1_2
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=8 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "****************************************************************************************************"
+echo "****************************************************************************************************"
+echo "****************************************************************************************************"
+echo "****************************************************************************************************"
+
 INPUTFILE=small.dat
 
 echo "****************************************************************************************************"
@@ -34,3 +75,47 @@ echo "NUMA-Auto-Mold-1-2-6"
 echo "****************************************************************************************************"
 export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_1_2
 TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=8 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "****************************************************************************************************"
+echo "****************************************************************************************************"
+echo "****************************************************************************************************"
+echo "****************************************************************************************************"
+echo "****************************************************************************************************"
+
+
+XITAO_PTT_PATH=/home/musabdu/xitao/ptt_layouts/puck
+INPUTFILE=big.dat
+
+echo "****************************************************************************************************"
+echo "RWSS"
+echo "****************************************************************************************************"
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=16 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-rwss ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Aware"
+echo "****************************************************************************************************"
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=16 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-No-Mold"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_1
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=16 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-Mold-2"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_2
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=16 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-Mold-6"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_6
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=16 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-Mold-1-2-6"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_1_2_6
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=16 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
+echo "****************************************************************************************************"
+echo "NUMA-Auto-Mold-1-2-6"
+echo "****************************************************************************************************"
+export XITAO_LAYOUT_PATH=${XITAO_PTT_PATH}/ptt_layout_1_2
+TAO_NOPLOT=1 AWIDTH=6 EXDECOMP=16 EYDECOMP=6 IXDECOMP=6 IYDECOMP=1 numactl -i all ../heat-tao-numa-auto ${INPUTFILE}
