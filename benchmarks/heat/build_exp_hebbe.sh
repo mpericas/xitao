7e70555091d4704f30513b9e5861a32c56a54ab5
Mustafa <musbar@gmail.com>
2020-04-21 16:59:40 +0200

    build experiment

---
+++ benchmarks/heat/build_exp_hebbe.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+export XITAO_STEALING_FREQ=2000
+CC=gcc
+CXX=g++
+rm heat-tao heat-tao-rwss heat-tao-numa*
+
+## RWSS ##
+cd ../../
+cp makefile.sched.rwss makefile.sched
+CXX=${CXX} CC=${CC} make lib
+cd benchmarks/heat/
+make clean; CXX=${CXX} CC=${CC} make 
+cp heat-tao heat-tao-rwss
+
+
+## NUMA ##
+cd ../../
+cp makefile.sched.numa makefile.sched
+CXX=${CXX} CC=${CC} make lib
+cd benchmarks/heat/
+make clean; CXX=${CXX} CC=${CC} make 
+cp heat-tao heat-tao-numa
+
+
+## NUMA Auto ##
+cd ../../
+cp makefile.sched.numa.auto makefile.sched
+CXX=${CXX} CC=${CC} make lib
+cd benchmarks/heat/
+make clean; CXX=${CXX} CC=${CC} make 
+cp heat-tao heat-tao-numa-auto
+
+
+## NUMA Auto Track STA ##
+cd ../../
+cp makefile.sched.numa.auto.sta.track makefile.sched
+CXX=${CXX} CC=${CC} make lib
+cd benchmarks/heat/
+make clean; CXX=${CXX} CC=${CC} make 
+cp heat-tao heat-tao-numa-auto-sta-track
+
